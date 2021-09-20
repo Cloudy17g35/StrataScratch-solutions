a4e25cc9dd82c6f856c545c44074f621e96b7487
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2021-09-20 12:39:19 +0200

    Create Salaries_Differences.sql

---
+++ Salaries_Differences.sql
@@ -0,0 +1,15 @@
+--Write a query that calculates the difference between the highest salaries 
+--found in the marketing and engineering departments. Output just the difference in salaries.
+--Tables: db_employee, db_dept
+
+SELECT
+(SELECT MAX(salary)
+FROM db_employee
+INNER JOIN db_dept
+ON db_employee.department_id = db_dept.id
+WHERE department = 'marketing')-
+(SELECT MAX(salary)
+FROM db_employee
+INNER JOIN db_dept
+ON db_employee.department_id = db_dept.id
+WHERE department = 'engineering') AS diff;
